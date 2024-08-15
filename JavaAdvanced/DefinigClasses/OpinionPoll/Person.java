efa885af9128c58c2097614573126327e66cba7f
Stanimir Stoew <145483234+sstoew93@users.noreply.github.com>
2024-08-15 14:32:36 +0300

    #oldhomeworks

---
+++ JavaAdvanced/DefinigClasses/OpinionPoll/Person.java
@@ -0,0 +1,27 @@
+package DefinigClasses.OpinionPoll;
+
+public class Person {
+    private String Name;
+    private int age;
+
+    public Person(String name, int age) {
+        Name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
