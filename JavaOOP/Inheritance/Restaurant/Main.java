a98b8d514c4095ab48d8af6250338699febaccc6
Stanimir Stoew <145483234+sstoew93@users.noreply.github.com>
2024-09-04 11:46:21 +0300

    #oldhomeworks

---
+++ JavaOOP/Inheritance/Restaurant/Main.java
@@ -0,0 +1,7 @@
+package Inheritance.Restaurant;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JavaOOP/Inheritance/Restaurant/Salmon.java
@@ -0,0 +1,11 @@
+package Inheritance.Restaurant;
+
+import java.math.BigDecimal;
+
+public class Salmon extends MainDish {
+    private static final double SALMON_GRAMS = 3;
+    public Salmon(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+        super.setGrams(SALMON_GRAMS);
+    }
+}
