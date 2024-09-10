5d54642cd87a955f9a5c81f87cab0afcf3a0c142
Stanimir Stoew <145483234+sstoew93@users.noreply.github.com>
2024-09-10 20:28:27 +0300

    #oldhomeworks

---
+++ JavaOOP/Inheritance/Zoo/Animal.java
@@ -0,0 +1,13 @@
+package Inheritance.Zoo;
+
+public class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
