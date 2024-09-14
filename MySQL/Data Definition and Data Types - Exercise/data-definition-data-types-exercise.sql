fafc97673ceeb08d10b4f61a87bae25d9a3cd24a
Stanimir Stoew <145483234+sstoew93@users.noreply.github.com>
2024-09-14 14:09:32 +0300

    Update data-definition-data-types-exercise.sql

--- MySQL/Data Definition and Data Types - Exercise/data-definition-data-types-exercise.sql
+++ MySQL/Data Definition and Data Types - Exercise/data-definition-data-types-exercise.sql
@@ -151,3 +151,92 @@ INSERT INTO `movies` (title, director_id, copyright_year, length, genre_id, cate
 ("Title4", 1, 1995, 130.12, 4, 3, 8.75, "test"),
 ("Title5", 4, 1996, 90.00, 5, 5, 7.90, "test");
 
+CREATE TABLE `categories` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `category` VARCHAR(15) NOT NULL,
+    `daily_rate` DOUBLE(5,2) NOT NULL,
+    `weekly_rate` DOUBLE(5,2) NOT NULL,
+    `monthly_rate` DOUBLE(5,2) NOT NULL,
+    `weekend_rate` DOUBLE(5,2) NOT NULL
+);
+
+INSERT INTO `categories` (category, daily_rate, weekly_rate, monthly_rate, weekend_rate) VALUES
+("category1", 55.00, 45.00, 48.00, 52.00),
+("category2", 55.00, 45.00, 48.00, 52.00),
+("category3", 55.00, 45.00, 48.00, 52.00);
+
+-- ****************************************************************
+
+CREATE DATABASE `car_rental`;
+
+CREATE TABLE `cars` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `plate_number` VARCHAR(15) NOT NULL,
+    `make` DOUBLE(5,2) NOT NULL,
+    `model` VARCHAR(15) NOT NULL,
+    `car_year` YEAR NOT NULL,
+    `category_id` INT REFERENCES categories(id),
+    `doors` INT NOT NULL,
+    `picture` BLOB NOT NULL,
+    `car_condition` VARCHAR(100) NOT NULL,
+    `available` BOOL NOT NULL
+);
+
+INSERT INTO `cars` (plate_number, make, model, car_year, category_id, doors, picture, car_condition, available) VALUES
+("FG321KD", 33.3, "model", "2020", 1, 4, "test", "good", true),
+("FG2321KD", 33.3, "model2", "2020", 2, 2, "test", "good", true),
+("FG3214KD", 33.3, "model3", "2020", 3, 2, "test", "good", true);
+
+CREATE TABLE `employees` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `first_name` VARCHAR(15) NOT NULL,
+    `last_name` VARCHAR(15) NOT NULL,
+    `title` VARCHAR(15) NOT NULL,
+    `notes` VARCHAR(100)
+);
+
+INSERT INTO `employees` (first_name, last_name, title) VALUES
+("name1", "lastname", "worker"),
+("name2", "lastname", "worker"),
+("name3", "lastname", "worker");
+
+
+CREATE TABLE `customers` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `driver_license_number` INT NOT NULL,
+    `full_name` VARCHAR(50) NOT NULL,
+    `address` VARCHAR(60) NOT NULL,
+    `city` VARCHAR(60) NOT NULL,
+    `zip_code` INT NOT NULL,
+    `notes` VARCHAR(100)
+);
+
+INSERT INTO `customers` (driver_license_number, full_name, address, city, zip_code) VALUES
+(32131321, "name name", "address", city, 6000),
+(32321131, "name name", "address", city, 6000),
+(32154631, "name name", "address", city, 6000);
+
+CREATE TABLE `rental_orders` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `employee_id` INT REFERENCES employees(id),
+    `customer_id` INT REFERENCES customers(id),
+    `car_id` INT REFERENCES cars(id),
+    `car_condition` VARCHAR(100) NOT NULL,
+    `tank_level` INT NOT NULL,
+    `kilometrage_start` INT NOT NULL,
+    `kilometrage_end` INT NOT NULL,
+    `total_kilometrage` INT NOT NULL,
+    `start_date` DATE NOT NULL,
+    `end_date` DATE NOT NULL,
+    `total_days` INT NOT NULL,
+    `rate_applied` DOUBLE NOT NULL,
+    `tax_rate` DOUBLE NOT NULL,
+    `order_status` VARCHAR(10) NOT NULL,
+    `notes` VARCHAR(100)
+);
+
+INSERT INTO `rental_orders` (employee_id, customer_id, car_id, car_condition, tank_level, kilometrage_start, kilometrage_end, total_kilometrage, start_date, end_date, total_days, rate_applied, tax_rate, order_status) VALUES
+(1, 2, 2, "good", "50", 321321, 54353, 654654, "2017-06-15", "2017-06-17", 2, 33.33, 22.22, "active"),
+(2, 1, 3, "good", "50", 321321, 54353, 654654, "2017-06-15", "2017-06-17", 2, 33.33, 22.22, "active"),
+(3, 3, 1, "good", "50", 321321, 54353, 654654, "2017-06-15", "2017-06-17", 2, 33.33, 22.22, "active");
+
