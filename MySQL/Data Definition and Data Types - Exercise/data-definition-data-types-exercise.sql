eeab8dd3395cb0bf2b15f28d652d8f0602ab67d7
Stanimir Stoew <145483234+sstoew93@users.noreply.github.com>
2024-09-14 14:57:55 +0300

    Update data-definition-data-types-exercise.sql

--- MySQL/Data Definition and Data Types - Exercise/data-definition-data-types-exercise.sql
+++ MySQL/Data Definition and Data Types - Exercise/data-definition-data-types-exercise.sql
@@ -129,19 +129,16 @@ INSERT INTO `categories` (category_name, notes) VALUES
 ("category4", "test"),
 ("category5", "test");
 
-CREATE TABLE `movies` (
-	`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `title` VARCHAR(50) NOT NULL,
-    `director_id` INT,
+CREATE TABLE `movies`(
+    `id`             INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `title`          VARCHAR(60)     NOT NULL,
+    `director_id`    INT REFERENCES directors (id),
     `copyright_year` YEAR,
-    `length` DOUBLE(5,2),
-    `genre_id` INT,
-    `category_id` INT,
-    `rating` DOUBLE(5,2),
-    `notes` TEXT,
-    FOREIGN KEY (`director_id`) REFERENCES `directors`(`id`),
-    FOREIGN KEY (`genre_id`) REFERENCES `genres`(`id`),
-    FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`)
+    `length`         DOUBLE(5, 2),
+    `genre_id`       INT REFERENCES genres (id),
+    `category_id`    INT REFERENCES categories (id),
+    `rating`         DOUBLE(5, 2),
+    `notes`          TEXT
 );
 
 INSERT INTO `movies` (title, director_id, copyright_year, length, genre_id, category_id, rating, notes) VALUES
@@ -150,6 +147,10 @@ INSERT INTO `movies` (title, director_id, copyright_year, length, genre_id, cate
 ("Title3", 3, 1994, 120.50, 1, 2, 9.50, "test"),
 ("Title4", 1, 1995, 130.12, 4, 3, 8.75, "test"),
 ("Title5", 4, 1996, 90.00, 5, 5, 7.90, "test");
+
+-- ****************************************************************
+
+CREATE DATABASE `car_rental`;
 
 CREATE TABLE `categories` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -164,10 +165,6 @@ INSERT INTO `categories` (category, daily_rate, weekly_rate, monthly_rate, weeke
 ("category1", 55.00, 45.00, 48.00, 52.00),
 ("category2", 55.00, 45.00, 48.00, 52.00),
 ("category3", 55.00, 45.00, 48.00, 52.00);
-
--- ****************************************************************
-
-CREATE DATABASE `car_rental`;
 
 CREATE TABLE `cars` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -240,3 +237,80 @@ INSERT INTO `rental_orders` (employee_id, customer_id, car_id, car_condition, ta
 (2, 1, 3, "good", "50", 321321, 54353, 654654, "2017-06-15", "2017-06-17", 2, 33.33, 22.22, "active"),
 (3, 3, 1, "good", "50", 321321, 54353, 654654, "2017-06-15", "2017-06-17", 2, 33.33, 22.22, "active");
 
+-- ***********************************************************************
+
+CREATE DATABASE `soft_uni`;
+
+CREATE TABLE `towns`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `addresses` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `address_text` VARCHAR(255) NOT NULL ,
+    `town_id` INT NOT NULL
+);
+
+ALTER TABLE `addresses`
+    ADD CONSTRAINT `fk_addresses_towns`
+	FOREIGN KEY (town_id) REFERENCES towns(id);
+
+CREATE TABLE `departments` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `employees` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `first_name` VARCHAR(255) NOT NULL,
+    `middle_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    `job_title` VARCHAR(255) NOT NULL,
+    `department_id` INT NOT NULL,
+    `hire_date` DATE NOT NULL,
+    `salary` DOUBLE(12,2) NOT NULL,
+    `address_id` INT
+);
+
+ALTER TABLE `employees`
+    ADD CONSTRAINT `fk_employees_department`
+	FOREIGN KEY (department_id) REFERENCES departments(id),
+    ADD CONSTRAINT `fk_employees_address`
+	FOREIGN KEY (address_id) REFERENCES addresses(id);
+
+INSERT INTO `towns` (name) VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+INSERT INTO `departments` (name) VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+INSERT INTO `employees` (first_name, middle_name, last_name, job_title, department_id, hire_date, salary) VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
+
+SELECT * FROM `towns`;
+SELECT * FROM `departments`;
+SELECT * FROM `employees`;
+
+SELECT * FROM `towns` ORDER BY name;
+SELECT * FROM `departments` ORDER BY name;
+SELECT * FROM `employees` ORDER BY salary DESC;
+
+SELECT name FROM `towns` ORDER BY name;
+SELECT name FROM `departments` ORDER BY name;
+SELECT first_name, last_name, job_title, salary FROM `employees` ORDER BY salary DESC;
+
+UPDATE `employees`
+SET salary = salary * 1.1;
+SELECT salary FROM `employees`;
