58b3a2478bf59b73c477405c3fa6c16f1141bc22
Stanimir Stoew <145483234+sstoew93@users.noreply.github.com>
2024-09-17 11:51:12 +0300

    #oldhomeworks

---
+++ JavaOOP/Polymorphism/MathOperation/Main.java
@@ -0,0 +1,9 @@
+package Polymorphism.MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new MathOperation().add(2, 2));
+        System.out.println(new MathOperation().add(3, 3, 3));
+        System.out.println(new MathOperation().add(4, 4, 4, 4));
+    }
+}
